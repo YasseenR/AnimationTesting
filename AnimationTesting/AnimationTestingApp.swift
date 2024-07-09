aeb13a9aa6a99f12092def793f71ae0ba7844db2
YasseenR <yasseenrouni@gmail.com>
2024-07-09 09:54:38 -0400

    Initial Commit

---
+++ AnimationTesting/AnimationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestingApp.swift
+//  AnimationTesting
+//
+//  Created by Yasseen Rouni on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
