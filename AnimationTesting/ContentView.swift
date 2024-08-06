073d7bea5574665ad75c07904774b7a6abdef382
YasseenR <yasseenrouni@gmail.com>
2024-08-06 16:12:43 -0400

    Created a stepper to animate button scale change.

--- AnimationTesting/ContentView.swift
+++ AnimationTesting/ContentView.swift
@@ -12,26 +12,24 @@ struct ContentView: View {
     
     
     var body: some View {
-        Button("Tap Me") {
-            //animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay (
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2-animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale Amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
     }
 }
